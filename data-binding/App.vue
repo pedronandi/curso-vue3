06e0503d7f5c4adbe6a5980395080331ee0475fa
Pedro Nandi <pedro.nandi@matera.com>
2021-11-15 21:12:25 -0300

    curso-vuejs3 single-file-components e data-binding

---
+++ data-binding/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 :style="{ textDecoration: decoration }">Hello {{ name }}</h1>
+    <input type="text" v-model="name">
+    <br>
+    <a :href="link">Link pro curso!</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({ /*data: two-way binding: O model interage com a view e vice-versa*/
+    name: 'Pedro',
+    link: 'https://treinamento.vuejsbrasil.org',
+    decoration: 'underline'
+  })
+}
+</script>
